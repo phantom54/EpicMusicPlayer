d6e0a8608461842ee0c575c8e1c29e2ba58b1bb7
phantom54 <35395849+phantom54@users.noreply.github.com>
2024-10-26 18:04:45 +0700

    Add files via upload

--- gamemusic/Epic.lua
+++ gamemusic/Epic.lua
@@ -589,4 +589,4 @@ local EpicMusicPlayer = LibStub("AceAddon-3.0"):GetAddon("EpicMusicPlayer")
 
 if not EpicMusicPlayer then return end
 
-EpicMusicPlayer:AddPlayList(nil, BestofWoW, false, true))
+EpicMusicPlayer:AddPlayList(nil, BestofWoW, false, true)
